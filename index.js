d80c5790ab677bc855b866fc79e218871fea4fa4
Powei <powei1990@gmail.com>
2021-04-25 21:45:23 +0800

    加了兩個按鈕，然後實測可開串流 但不知道怎麼下指令開串流

--- index.js
+++ index.js
@@ -5,50 +5,79 @@ const io = require('socket.io')(server);
 var exec = require('child_process').exec;
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/www/index.html');
+    res.sendFile(__dirname + '/www/index.html');
 })
 io.on('connection', (socket) => {
-	console.log(socket.id);
-	socket.on('user', function(data) {
-		console.log(socket.id + " : " + data.text);
-	})
+    console.log(socket.id);
+    socket.on('user', function(data) {
+        console.log(socket.id + " : " + data.text);
+    })
 
-	socket.on('ledon', function() {
-		ledon(socket);
-		console.log("socket on ledon");
-	})
+    socket.on('ledon', function() {
+        ledon(socket);
+        console.log("socket on ledon");
+    })
 
-	socket.on('ledoff', function() {
-		ledoff(socket);
-		console.log("socket on ledoff")
-	})
+    socket.on('streamOn', function() {
+        streamOn(socket);
+        console.log("socket on streamOn")
+    })
+
+    socket.on('streamOff', function() {
+        streamOff(socket);
+        console.log("socket on streamOff")
+    })
 
 
 });
 
 
 function ledon(socket) {
-	var cmd = 'python ~/DadFarm/controlgpio.py -p 11 -o 1';
-	exec(cmd, function(error, stdout, stderr) {
-		if (error != null) {
-			console.log("出錯了！" + error);
-			throw error;
-		} else {
-			console.log(stdout);
-		}
-	});
+    var cmd = 'python ~/DadFarm/controlgpio.py -p 11 -o 1';
+    exec(cmd, function(error, stdout, stderr) {
+        if (error != null) {
+            console.log("出錯了！" + error);
+            throw error;
+        } else {
+            console.log(stdout);
+        }
+    });
 }
 
 function ledoff(socket) {
-	var cmd = 'python ~/DadFarm/controlgpio.py -p 11 -o 0';
-	exec(cmd, function(error, stdout, stderr) {
-		if (error != null) {
-			console.log("出錯了！" + error);
-			throw error;
-		} else {
-			console.log(stdout);
-		}
-	});
+    var cmd = 'python ~/DadFarm/controlgpio.py -p 11 -o 0';
+    exec(cmd, function(error, stdout, stderr) {
+        if (error != null) {
+            console.log("出錯了！" + error);
+            throw error;
+        } else {
+            console.log(stdout);
+        }
+    });
+}
+
+function streamOn(socket) {
+    var cmd = 'cd mjpg-streamer/mjpg-streamer-experimental/ ';
+    exec(cmd, function(error, stdout, stderr) {
+        if (error != null) {
+            console.log("出錯了！" + error);
+            throw error;
+        } else {
+            console.log(stdout);
+        }
+    });
+}
+
+function streamOff(socket) {
+    var cmd = 'kill $(pgrep mjpg)';
+    exec(cmd, function(error, stdout, stderr) {
+        if (error != null) {
+            console.log("出錯了！" + error);
+            throw error;
+        } else {
+            console.log(stdout);
+        }
+    });
 }
 
 server.listen(3000);
