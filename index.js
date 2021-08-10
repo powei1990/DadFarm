317a1b5c078edc68061d24257fe06a93792f9e5c
Powei <powei1990@gmail.com>
2021-08-10 23:28:01 +0800

    socket.io pass value test success

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const io = require('socket.io')(server);
 var dht22 = require('./models/dht22');
 var exec = require('child_process').exec;
 
+const schedule = require('node-schedule');
+const rule = new schedule.RecurrenceRule();
+
 var Gpio = require('onoff').Gpio;
-var LED = new Gpio(17, 'out'); 
+var LED = new Gpio(17, 'out');
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    
+
     dht22.find().skip(dht22.count() - 100)
         .exec(function(err, docs) {
             if (err || !docs) {
@@ -20,7 +23,7 @@ app.get('/', function(req, res) {
                 var isLedOn = LED.readSync();
                 console.log(isLedOn);
             }
-            res.render('index', { docs: docs ,isLedOn:isLedOn});
+            res.render('index', { docs: docs, isLedOn: isLedOn });
         });
 
 
@@ -28,7 +31,7 @@ app.get('/', function(req, res) {
 
 io.on('connection', (socket) => {
     console.log(socket.id);
-    
+
     socket.on('user', function(data) {
         console.log(socket.id + " : " + data.text);
     })
@@ -43,18 +46,31 @@ io.on('connection', (socket) => {
         console.log("socket on ledon");
     })
 
+    socket.on('setTime', function(timeValue) {
+        console.log("Set Time : " + timeValue);
+        SetTime(timeValue);
+    })
+
 
 });
 
 
 function ledon(socket) {
     LED.writeSync(1);
-   console.log("function ledon");
+    console.log("function ledon");
 }
 
 function ledoff(socket) {
     LED.writeSync(0);
- console.log("function ledoff");
+    console.log("function ledoff");
+}
+
+function SetTime(timeValue) {
+    rule.minute = timeValue;
+    console.log("In SetTime Function! at rule :" + rule.minute)
+    const job = schedule.scheduleJob(rule, function() {
+        console.log('The answer to life, the universe, and everything!');
+    });
 }
 
 
